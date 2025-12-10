8f3f8b26e3e08916c61401438390350c42937a8b
Vishal Vashishth <vishal112vp@gmail.com>
2025-12-10 23:37:10 +0530

    NewsBoard

--- src/Component/NewsBord.jsx
+++ src/Component/NewsBord.jsx
@@ -1,9 +1,19 @@
+import { use } from "react";
 
 
 const NewsBord = () => {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url).than(responce=>responce.json()).than(data=>setArticles(data.articles)
+    );
+  }, []);
+
   return (
     <div>
-      <h2 className="text-center">Latest <span className="">News</span></h2>
+      <h2 className="text-center">Latest <span className="badge bg-danger ">News</span></h2>
     </div>
   )
 }
